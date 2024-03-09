61a9260d068d941a58fb2975e6266414cd3f42a9
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-03-08 23:53:38 -0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,38 @@ import { Profile } from './components/Profile.tsx'
 
 export function App() {
     return (
-        <header>
-            <img src={cover} alt="" />
-            <div className="mx-auto max-w-[min(1280px,100vw-20px)]">
-                <Profile />
+        <div>
+            <header>
+                <img src={cover} alt="" draggable={false}/>
+            </header>
+
+            <div className="mx-auto max-w-[min(864px,100vw-20px)]">
+                <Profile/>
+
+                <div>
+                    <div>
+                        <h3>Publicações</h3>
+                        <span>6 publicações</span>
+                    </div>
+                    <input placeholder="Buscar contaúdo"/>
+                </div>
+
+                <div className="grid grid-cols-2 gap-[2rem]">
+                    <div>
+                        <header>
+                            <h3>Javascript data types and data structures</h3>
+                            <span>Há 1 dia</span>
+                        </header>
+                        <p>
+                            Programming languages all have built-in data structures, but these often differ from one
+                            language to another.
+                            This article attempts to list the built-in data structures available in JavaScript and what
+                            properties they have.
+                            These can be used to build other data structures. Wherever possible, comparisons with other
+                            languages are drawn.
+                        </p>
+                    </div>
+                </div>
             </div>
-        </header>
-    )
+        </div>)
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,32 +1,37 @@
-import { Buildings, GithubLogo, Users } from '@phosphor-icons/react'
+import { ArrowSquareOut, Buildings, GithubLogo, Users } from '@phosphor-icons/react'
 
 import profileImage from './../assets/profileImage.png'
 
 export function Profile() {
     return (
-        <div className="flex items-center justify-between gap-[2rem] rounded-[10px] bg-base-profile px-[2.5rem] py-[2rem]">
-            <img src={profileImage} alt="Imagem do Perfil Github" />
-            <div className="flex flex-col">
-                <h2>Cameron Williamsom</h2>
-                <span>
+        <div className="relative mt-[-5rem] flex items-center justify-between gap-[2rem] rounded-[10px] bg-base-profile px-[2.5rem] py-[2rem]">
+            <img src={profileImage} alt="Imagem do Perfil Github" width={148} draggable={false} />
+            <div className="flex flex-col gap-[0.5rem]">
+                <div className="flex items-center justify-between gap-[0.5rem]">
+                    <h2 className="text-[1.5rem] text-base-title">Cameron Williamsom</h2>
+                    <a href="aaa" className="flex gap-[0.5rem] text-blue">
+                        GITHUB
+                        <ArrowSquareOut size={18} />
+                    </a>
+                </div>
+                <span className="leading-[1.5rem]">
                     Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam
                     dignissim aenean malesuada suscipit.Nunc, volutpat pulvinar vel mass.
                 </span>
-                <div className="flex gap-[1.5rem]">
-                    <div className="flex">
+                <div className="mt-[0.75rem] flex gap-[1.5rem]">
+                    <div className="flex items-center gap-[0.5rem]">
                         <GithubLogo size={18} className="text-base-label"/>
                         <span className="text-base-subtitle">cameronwll</span>
                     </div>
-                    <div className="flex">
+                    <div className="flex items-center gap-[0.5rem]">
                         <Buildings size={18} className="text-base-label"/>
                         <span className="text-base-subtitle">Rocketseat</span>
                     </div>
-                    <div className="flex">
+                    <div className="flex items-center gap-[0.5rem]">
                         <Users size={18} className="text-base-label"/>
                         <span className="text-base-subtitle">32 seguidores</span>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </div>)
 }
