5850dbe089836a5503297d6867a88d544600e040
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-03-13 23:49:08 -0300

    filter issues list

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -9,6 +9,10 @@ interface IIssue {
 
 export function PostsList() {
     const [issues, setIssues] = useState<IIssue[]>([])
+    const [search, setSearch] = useState('')
+
+    const filteredIssues = search.length > 0 ? issues.filter((filter) => filter.title.includes(search)) : issues
+
     async function fetchIssues() {
         const response = await fetch('https://api.github.com/search/issues?q=repo:lucasdebeterco/github-blog')
         const data = await response.json()
@@ -18,7 +22,7 @@ export function PostsList() {
 
     useEffect(() => {
         fetchIssues()
-    }, [fetchIssues])
+    }, [])
 
     return (
         <>
@@ -30,12 +34,13 @@ export function PostsList() {
                 <input
                     className="w-full rounded-[6px] border-[1px] border-base-border bg-base-input p-[1rem] placeholder:text-base-label"
                     placeholder="Buscar contaúdo"
+                    onChange={(e) => setSearch(e.target.value)}
                 />
             </div>
 
             <div className="my-[3rem] grid grid-cols-[repeat(auto-fit,minmax(416px,1fr))] gap-[2rem]">
-                {issues.map((issue) => (
-                    <div className="flex flex-col gap-[1.25rem] rounded-[10px] bg-base-post p-[2rem]">
+                {filteredIssues.map((issue) => (
+                    <div className="flex flex-col gap-[1.25rem] rounded-[10px] bg-base-post p-[2rem]" key={issue.title}>
                         <header className="flex justify-between">
                             <h3 className="text-[1.25rem] text-base-title">{issue.title}</h3>
                             <span className="whitespace-nowrap text-[0.875rem] text-base-span">{issue.created_at} Há 1 dia</span>
